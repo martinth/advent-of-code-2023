1cb68fa2fa71216b288a6a2683a6dadbe74972db
Martin Thurau <martin.thurau@gmail.com>
2023-12-03 11:19:41 +0100

    Improve day template

--- src/_day_tpl/main.rs
+++ src/_day_tpl/main.rs
@@ -1,27 +1,49 @@
-use advent_of_code_2023::common::read_valid_lines;
+use std::fs::read_to_string;
 use anyhow::{Result, Context};
+use aoc_parse::{parser, prelude::*};
 
 
-fn solve(filename: &str) -> Result<u32> {
-    let lines = read_valid_lines(filename);
+fn parse_input(filename: &str) -> Result<_> {
+    let parser = parser!();
+
+    let raw_data = read_to_string(filename)?;
+    parser.parse(&raw_data).context("parse error")
+}
+
+fn solve_part_1(filename: &str) -> Result<u32> {
+    let input = parse_input(filename)?;
+    println!("{:?}", input);
 
     todo!()
 }
 
-fn main() {
-    let result = solve("src/day_xx/input.txt");
+fn solve_part_2(filename: &str) -> Result<u32> {
+    let input = parse_input(filename)?;
+    println!("{:?}", input);
 
-    println!("Result: {}", result.expect("result should exist"));
+    todo!()
+}
+
+fn main() -> Result<()> {
+    println!("Result part 1: {}", solve_part_1("src/day_xx/input.txt")?);
+    println!("Result part 2: {}", solve_part_2("src/day_xx/input.txt")?);
+    Ok(())
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::solve;
+    use crate::{solve_part_1, solve_part_2};
 
     #[test]
-    fn solve_test_input() {
-        let result = solve("src/day_xx/test_input.txt").unwrap();
+    fn solve_test_input_1() {
+        let result = solve_part_1("src/day_xx/test_input.txt").unwrap();
+        assert_eq!(result, 42);
+    }
+
+    #[test]
+    fn solve_test_input_2() {
+        let result = solve_part_2("src/day_xx/test_input.txt").unwrap();
         assert_eq!(result, 42);
     }
 }
