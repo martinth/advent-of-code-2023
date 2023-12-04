0bb3e009254256708ed1c80a2df73a9ffdb48e4b
Martin Thurau <martin.thurau@gmail.com>
2023-12-04 14:21:50 +0100

    Update template

--- src/_day_tpl/main.rs
+++ src/_day_tpl/main.rs
@@ -1,32 +1,39 @@
-use std::fs::read_to_string;
 use anyhow::{Result, Context};
-use aoc_parse::{parser, prelude::*};
+#[macro_use]
+extern crate simple_log;
+
+mod parse {
+    use aoc_parse::{parser, prelude::*};
+    use anyhow::{Result, Context};
+    use std::fs::read_to_string;
 
 
-fn parse_input(filename: &str) -> Result<_> {
-    let parser = parser!();
+    pub fn parse_input(filename: &str) -> Result<_> {
+        let parser = parser!();
 
-    let raw_data = read_to_string(filename)?;
-    parser.parse(&raw_data).context("parse error")
+        let raw_data = read_to_string(filename)?;
+        parser.parse(&raw_data).context("parse error")
+    }
 }
 
 fn solve_part_1(filename: &str) -> Result<u32> {
-    let input = parse_input(filename)?;
+    let input = parse::parse_input(filename)?;
     println!("{:?}", input);
 
     todo!()
 }
 
 fn solve_part_2(filename: &str) -> Result<u32> {
-    let input = parse_input(filename)?;
+    let input = parse::parse_input(filename)?;
     println!("{:?}", input);
 
     todo!()
 }
 
 fn main() -> Result<()> {
-    println!("Result part 1: {}", solve_part_1("src/day_xx/input.txt")?);
-    println!("Result part 2: {}", solve_part_2("src/day_xx/input.txt")?);
+    simple_log::quick!("info");
+    info!("Result part 1: {}", solve_part_1("src/day_xx/input.txt")?);
+    info!("Result part 2: {}", solve_part_2("src/day_xx/input.txt")?);
     Ok(())
 }
 
